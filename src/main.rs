fd443e656ed5c0bffc03d839c32b652ae6cb534c
Jonathan E. Magen <yonkeltron@gmail.com>
2019-11-17 19:29:52 -0500

    jawn progress bar
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::{App, Arg};
+use std::thread;
+use std::time::Duration;
+
+use indicatif::{ProgressBar, ProgressStyle};
 
 mod jawnfile;
 
@@ -7,5 +11,34 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("jawnfile")
+                .short("j")
+                .long("jawnfile")
+                .value_name("PATH")
+                .help("specify an alternative Jawnfile")
+                .takes_value(true),
+        )
         .get_matches();
+
+    let jawnfile_path = match matches.value_of("jawnfile").unwrap_or("") {
+        "" => "Jawnfile",
+        path => path,
+    };
+
+    println!("Jawnfile path: {}", jawnfile_path);
+
+    let pb = ProgressBar::new(1024);
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template("{spinner:.green} [{elapsed_precise}] [{bar:50.cyan/blue}] ({pos}/{len})")
+            .progress_chars("#>-"),
+    );
+
+    for _i in 0..1000 {
+        thread::sleep(Duration::from_millis(5));
+        pb.inc(1);
+    }
+
+    pb.finish_with_message("jawn complete");
 }
